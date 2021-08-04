d31bc6b651b7e481e23febd8f821e4ff1dd3914f
MaliciaMrg <maliciamrg@users.noreply.github.com>
2021-08-05 00:01:07 +0200

    trace array of string              version 1.0.4

--- src/main/java/com/malicia/mrg/util/Output.java
+++ src/main/java/com/malicia/mrg/util/Output.java
@@ -13,13 +13,15 @@ import java.text.NumberFormat;
 public class Output implements StreamingProcessOwner {
 
     private static final Logger LOGGER = LogManager.getLogger(Output.class);
+    public String resumer = "";
+    public String[] resumerString;
+    public String[] traceString;
     NumberFormat formatter = new DecimalFormat("00000");
     private int i = 0;
-    public String resumer ="";
-    public String[] resumerString;
 
-    public Output(String[] resumerString) {
+    public Output(String[] resumerString, String[] traceString) {
         this.resumerString = resumerString;
+        this.traceString = traceString;
     }
 
     public StreamingProcessOutputType getOutputType() {
@@ -30,21 +32,32 @@ public class Output implements StreamingProcessOwner {
         if (stdout) {
             i++;
             String lline = "[" + formatter.format(i) + "] " + line;
+
             boolean resumeEnd = false;
             for (String element : resumerString) {
                 resumeEnd = resumeEnd || line.contains(element);
             }
-            if (resumeEnd || line.contains("deleting")) {
+            boolean traceEnd = false;
+            for (String element : traceString) {
+                resumeEnd = resumeEnd || line.contains(element);
+            }
+
+            if (resumeEnd) {
+
                 LOGGER.info(lline);
                 if (resumeEnd) {
                     resumer = resumer + lline + "\n";
                 }
+
             } else {
-                if ((i % 10) == 0) {
+
+
+                if (traceEnd) {
                     LOGGER.info(lline);
                 } else {
                     LOGGER.debug(lline);
                 }
+
             }
         } else {
             LOGGER.error(line);
